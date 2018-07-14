018badbdc1861472ac7a6417aeb986c7e4b0f395
i-nino <i.nino@protonmail.com>
2018-07-14 14:29:54 +0200

    Update KApcInjection.cpp

--- APCs/KApcInjection/KApcInjection.cpp
+++ APCs/KApcInjection/KApcInjection.cpp
@@ -7,18 +7,9 @@ extern "C" {
 using _LoadLibrary = PVOID(__stdcall*)(LPCWSTR);
 _LoadLibrary LoadLibrary;
 
-typedef struct
-{
-	WORK_QUEUE_ITEM WorkItem;
-	KEVENT Event;
-	PKTHREAD Thread;
-	PEPROCESS Process;
-	HANDLE Pid;
-} WRKITM_INJECTION, *PWRKITEM_INJECTION;
-
 
 VOID
-ApcInjectionRoutine2(
+ApcInjectionRoutine(
 	PKAPC KApc,
 	PKNORMAL_ROUTINE*,
 	PVOID*, PVOID*, PVOID*
@@ -31,39 +22,40 @@ ApcInjectionRoutine2(
 	wchar_t* DllBuffer {};
 	ULONG_PTR DllLen { 512 };
 
-	if (NT_SUCCESS(ZwAllocateVirtualMemory(ZwCurrentProcess(),
-										   reinterpret_cast<PVOID*>(&DllBuffer),
-										   0,
-										   &DllLen,
-										   MEM_COMMIT,
-										   PAGE_READWRITE))) 
+	if (NT_SUCCESS(ZwAllocateVirtualMemory(ZwCurrentProcess(), 
+	                                       reinterpret_cast<PVOID*>(&DllBuffer),
+					       0, 
+					       &DllLen,
+					       MEM_COMMIT,
+					       PAGE_READWRITE))) 
 	{
 		KAPC_STATE ApcState;
 		KeStackAttachProcess(IoGetCurrentProcess(), &ApcState);
 		RtlStringCbCopyW(DllBuffer, DllLen, L"ntoskrnl.exe");
 		KeUnstackDetachProcess(&ApcState);
 
-		auto Apc = static_cast<PKAPC>(ExAllocatePoolWithTag(NonPagedPool,
-															sizeof(KAPC),
-															KAPC_TAG));
+		auto Apc = static_cast<PKAPC>(ExAllocatePoolWithTag(NonPagedPool, 
+								    sizeof(KAPC), 
+								    KAPC_TAG));
 		auto ApcCleanup = [](PKAPC Apc, PKNORMAL_ROUTINE*,
-							 PVOID*, PVOID*, PVOID*)
+					PVOID*, PVOID*, PVOID*)
 		{
 			ExFreePoolWithTag(Apc, KAPC_TAG);
 		};
 
 		KeInitializeApc(Apc,
-						KeGetCurrentThread(),
-						OriginalApcEnvironment,
-						ApcCleanup,
-						nullptr,
-						reinterpret_cast<PKNORMAL_ROUTINE>(LoadLibrary),
-						UserMode,
-						DllBuffer);
-		if (!KeInsertQueueApc(Apc,
-							  nullptr,
-							  nullptr,
-							  IO_NO_INCREMENT)) {
+				KeGetCurrentThread(),
+				OriginalApcEnvironment,
+				ApcCleanup,
+				nullptr,
+				reinterpret_cast<PKNORMAL_ROUTINE>(LoadLibrary),
+				UserMode,
+				DllBuffer);
+		if (!KeInsertQueueApc(Apc, 
+		                      nullptr,
+				      nullptr,
+				      IO_NO_INCREMENT)) 
+		{
 			ExFreePoolWithTag(Apc, KAPC_TAG);
 			dprintf("Failed to queue UserApc\n");
 		}
@@ -90,8 +82,8 @@ ApcImageCallback(
 				return;
 			ULONG Rva;
 			auto Status = Injection::KGetRoutineAddressFromModule(L"\\SystemRoot\\System32\\Kernel32.dll",
-																  "LoadLibraryW",
-																  &Rva);
+									      "LoadLibraryW",
+									      &Rva);
 			if (!NT_SUCCESS(Status))
 				return;
 
@@ -99,16 +91,16 @@ ApcImageCallback(
 			dprintf("LoadLibrary: 0x%p\n", LoadLibrary);
 #if defined(INJECTION)
 			auto Apc = (PKAPC) ExAllocatePoolWithTag(NonPagedPool,
-													 sizeof(KAPC),
-													 KAPC_TAG);
+								 sizeof(KAPC),
+								 KAPC_TAG);
 			KeInitializeApc(Apc,
-							KeGetCurrentThread(),
-							OriginalApcEnvironment,
-							(PKKERNEL_ROUTINE) ApcInjectionRoutine2,
-							nullptr,
-							nullptr,
-							KernelMode,
-							nullptr);
+					KeGetCurrentThread(),
+					OriginalApcEnvironment,
+					(PKKERNEL_ROUTINE) ApcInjectionRoutine,
+					nullptr,
+					nullptr,
+					KernelMode,
+					nullptr);
 			if (!KeInsertQueueApc(Apc, nullptr, nullptr, IO_NO_INCREMENT)) {
 				ExFreePoolWithTag(Apc, KAPC_TAG);
 				return;
@@ -116,8 +108,8 @@ ApcImageCallback(
 #else
 			/* APC test to see where the NormalRoutine will get executed */
 			auto kApc = (PKAPC) ExAllocatePoolWithTag(NonPagedPool,
-													  sizeof(KAPC),
-													  KAPC_TAG);
+								  sizeof(KAPC),
+								  KAPC_TAG);
 
 			auto KernelRoutine = [](PKAPC Apc, PKNORMAL_ROUTINE*, PVOID*, PVOID*, PVOID*)
 			{
@@ -130,13 +122,13 @@ ApcImageCallback(
 			};
 
 			KeInitializeApc(kApc,
-							KeGetCurrentThread(),
-							OriginalApcEnvironment,
-							KernelRoutine,
-							nullptr,
-							(PKNORMAL_ROUTINE) LoadLibrary,
-							UserMode,
-							nullptr);
+					KeGetCurrentThread(),
+					OriginalApcEnvironment,
+					KernelRoutine,
+					nullptr,
+					(PKNORMAL_ROUTINE) LoadLibrary,
+					UserMode,
+					nullptr);
 			KeInsertQueueApc(kApc, 0, 0, IO_NO_INCREMENT);
 #endif
 		}
